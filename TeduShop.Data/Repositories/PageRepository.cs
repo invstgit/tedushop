1bb5b862149c8c8877f3443e070e09efb40814f7
bachdv <invst@outlook.com>
2016-05-22 20:17:48 +0700

    Add Data.Repositories

---
+++ TeduShop.Data/Repositories/PageRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data.Repositories
+{
+    public interface IPageRepository : IRepository<Page>
+    {
+        IEnumerable<Page> GetByAlias(string alias);
+    }
+
+    public class PageRepository : RepositoryBase<Page>, IPageRepository
+    {
+        public PageRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+
+        public IEnumerable<Page> GetByAlias(string alias)
+        {
+            return this.DbContext.Pages.Where(x => x.Alias == alias);
+        }
+    }
+}
